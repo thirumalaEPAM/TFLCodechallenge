f28d56b4b7c503848f328b977e3282fb1d6fff48
thirumalaEPAM <thirumala_rajolu@epam.com>
2022-05-26 05:08:16 +0530

    new changes

---
+++ run.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Thirumala_Rajolu\.nuget\packages\nunit.consolerunner\3.15.0\tools
+
+nunit3-console.exe "C:\Thirumala\PersonalDocs\TFLBDDFramework\TFLBDDFramework\bin\Debug\net6.0\TFLBDDFramework.dll"
